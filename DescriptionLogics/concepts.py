708e68b9414d2b57ffc2bd6a18ff325620927eb2
MihaelaGaman <mp.gaman@gmail.com>
2016-11-10 17:59:23 +0200

    Added eval for concepts.

--- DescriptionLogics/concepts.py
+++ DescriptionLogics/concepts.py
@@ -1,13 +1,135 @@
 from helpers import *
 
-def get_recursive_pred(formula):
-    p = get_premises(formula)
 
-    for pi in p[0][1]:
-        if not isinstance(pi[1], list):
-                return pi[1]
-        else:
-            get_recursive_pred(pi[1][1])
+def get_recursive_pred(formula, l):
+    laux = deepcopy(l)
+    if is_rule(formula):
+        p = get_premises(formula)
+    else:
+        p = get_args(formula)
+
+    for pi in p:
+        if pi[0] == 'pred':
+            laux.append(pi)
+        elif pi[0] != 'fct':
+            laux = get_recursive_pred(pi, laux)
+
+    return laux
+
+
+def prove_concept(concept, abox_kb, tbox_kb):
+    pass
+
+
+def included_in(c, concepts):
+    for c1 in concepts:
+        if c1[1] == c[1]:
+            return True
+
+    return False
+
+def get_defined_concepts(tbox_kb):
+    concepts = []
+    print "Defined symbols:"
+
+    for f in tbox_kb:
+        c = get_conclusion(f)
+        concepts.append(c)
+        print c[1]
+
+    return concepts
+
+def get_base_concepts(tbox_kb):
+    defined = get_defined_concepts(tbox_kb)
+    base = []
+
+    print "Base symbols: "
+    for formula in tbox_kb:
+        lc = get_recursive_pred(formula, [])
+        for c in lc:
+            if not included_in(c, defined) and not included_in(c, base):
+                base.append(c)
+                print c[1]
+
+    return base
+
+def get_true_premises(premise, l):
+    laux = deepcopy(l)
+    preds = get_args(premise)
+
+    for p in preds:
+        if p[0] == 'pred' and p[1] not in laux:
+            laux.append(p[1])
+        elif p[0] == 'and':
+            laux = get_true_premises(p, laux)
+
+    return laux
+
+
+def prove_all(abox_kb, tbox_kb):
+    local_abox = deepcopy(abox_kb)
+    local_tbox = deepcopy(tbox_kb)
+
+    defined = get_defined_concepts(tbox_kb)
+    base = get_base_concepts(tbox_kb)
+
+    to_be_proved = deepcopy(defined + base)
+
+    constants = []
+
+    # Get all the constants in ABOX
+    for a in local_abox:
+        for const in a[2]:
+            c = make_const(const[1])
+            if c not in constants:
+                constants.append(c)
+
+    print "Constants: ", constants
+
+
+    # Get premises from theorem
+    changed = True
+    while changed:
+        aux_abox = deepcopy(local_abox)
+        changed = False
+
+        for formula in local_tbox:
+            hypo = get_conclusion(formula)
+            premise = get_premises(formula)
+
+            for a in aux_abox:
+                if a[1] == hypo[1]:
+                    arg = make_const(a[2][0][1])
+                    for p in premise:
+                        true_concepts = get_true_premises(p, [])
+                        for name in true_concepts:
+                            atom = make_atom(name, arg)
+                            if atom not in local_abox:
+                                changed = True
+                                local_abox.append(atom)
+
+            # Get true theorems
+            for const in constants:
+                for p in premise:
+                    true_concepts = get_true_premises(p, [])
+                    ok = True
+                    for name in true_concepts:
+                        atom = make_atom(name, const)
+                        if atom not in local_abox:
+                            ok = False
+                    if ok == True:
+                        ta = make_atom(hypo[1], const)
+                        if ta not in local_abox:
+                            changed = True
+                            local_abox.append(ta)
+
+
+    print_KB(local_abox)
+    print_KB(abox_kb)
+
+    #print forward_chaining(deepcopy(local_abox + local_tbox), make_atom("Mother", make_const("MARY")))
+
+    # Get theorem from premises
 
 
 def main():
@@ -25,56 +147,58 @@ def main():
 
     add_statement(tbox_kb, make_atom('Mother', make_var('x')),\
                             make_and(make_atom('Woman', make_var('x')),\
-                                make_and(make_atom("hasChild", make_var("x"), make_var("y")),\
+                                make_and(make_function_call("hasChild", make_var("x"), make_var("y")),\
                                 make_atom("Person", make_var("y")))))
     
     
     add_statement(tbox_kb, make_atom('Father', make_var('f')),\
                             make_and(make_atom('Man', make_var('f')),\
-                                make_and(make_atom("hasChild", make_var("f"), make_var("y")),\
-                                make_atom("Person", make_var("y")))))
+                                make_and(make_function_call("hasChild", make_var("f"), make_var("c")),\
+                                make_atom("Person", make_var("c")))))
 
-    add_statement(tbox_kb, make_atom('Parent', make_var('f')),\
-                            make_or(make_atom('Father', make_var('f')),\
-                                make_atom("Mother", make_var("y"))))
+    add_statement(tbox_kb, make_atom('Parent', make_var('p')),\
+                            make_or(make_atom('Father', make_var('p')),\
+                                make_atom("Mother", make_var("p"))))
 
-    add_statement(tbox_kb, make_atom('Grandmother', make_var('x')),\
-                            make_and(make_atom('Mother', make_var('x')),\
-                                make_and(make_atom("hasChild", make_var("x"), make_var("y")),\
-                                make_atom("Parent", make_var("y")))))
+    add_statement(tbox_kb, make_atom('Grandmother', make_var('g')),\
+                            make_and(make_atom('Mother', make_var('g')),\
+                                make_and(make_function_call("hasChild", make_var("g"), make_var("z")),\
+                                make_atom("Parent", make_var("z")))))
 
-    # To be added:
-    # MotherWithManyChildren = Mother and moreThan(hasChildren, 3)
-    # MotherWithoutDaughter = Mother and Any(hasChild(not Woman))
-    # Wife = Woman and Exists(hasHusband(Man))
+    add_statement(tbox_kb, make_atom('MotherWithManyChildren', make_var("mwmc")), \
+                            make_and(make_atom('Mother', make_var('mwmc')), \
+                                make_and(make_function_call('hasChild', make_var('mwmc'), make_var('c1')), \
+                                    make_and(make_function_call('hasChild', make_var('mwmc'), make_var('c2')),\
+                                        make_function_call('hasChild', make_var('mwmc'), make_var('c3'))))))
 
+    add_statement(tbox_kb, make_atom('MotherWithoutDaughter', make_var('x')),\
+                            make_and(make_atom('Woman', make_var('x')),\
+                                make_neg(make_and(make_function_call("hasChild", make_var("x"), make_var("y")),\
+                                make_atom("Woman", make_var("y"))))))
 
+    add_statement(tbox_kb, make_atom('Wife', make_var('x')),\
+                        make_and(make_atom('Woman', make_var('x')),\
+                            make_and(make_function_call("hasHusband", make_var("x"), make_var("y")),\
+                            make_atom("Man", make_var("y")))))
+
+    KB = deepcopy(tbox_kb)
 
 
     # ABox
     abox_kb.append(make_atom('MotherWithoutDaughter', make_const("MARY")))
     abox_kb.append(make_atom('Father', make_const("PETER")))
-    abox_kb.append(make_atom('hasChild', make_const("PETER"), make_const("HARRY")))
-    abox_kb.append(make_atom('hasChild', make_const("MARY"), make_const("PETER")))
-    abox_kb.append(make_atom('hasChild', make_const("MARY"), make_const("PAUL")))
+    abox_kb.append(make_function_call('hasChild', make_const("PETER"), make_const("HARRY")))
+    abox_kb.append(make_function_call('hasChild', make_const("MARY"), make_const("PETER")))
+    abox_kb.append(make_function_call('hasChild', make_const("MARY"), make_const("PAUL")))
 
-    print_KB(abox_kb)
-    print_KB(tbox_kb)
+    for atom in abox_kb:
+        KB.append(atom)
 
-    concepts = []
-    print "Defined symbols:"
-    for f in tbox_kb:
-        c = get_conclusion(f)[1]
-        concepts.append(c)
-        print c
+    # print_KB(abox_kb)
+    # print_KB(tbox_kb)
 
-    print "Base symbols: "
-    # for f in tbox_kb:
-    #   p = get_premises(f)
-    #   for pi in p[0][1]:
-    #       if pi[1] not in concepts:
-    #           print pi[1]
-    for formula in tbox_kb:
-       print get_recursive_pred(formula)
+    #print forward_chaining(deepcopy(KB), make_atom('Parent', make_const("PETER")))
+
+    prove_all(abox_kb, tbox_kb)
 
 main()
--- DescriptionLogics/helpers.py
+++ DescriptionLogics/helpers.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def is_negative_literal(n):
     if n[0] == "neg":
         return True
@@ -7,6 +9,35 @@ def is_positive_literal(n):
     if n[0] != "neg":
         return True
     return False
+
+
+def get_premises(formula):
+    # TODO
+    #return []
+    return [get_args(n)[0] for n in get_args(formula) if is_negative_literal(n)]
+    
+def get_conclusion(formula):
+    # TODO
+    #pass
+    return list(filter(is_positive_literal, get_args(formula)))[0]
+
+def is_fact(formula):
+    # TODO
+    #pass
+    return is_positive_literal(formula)
+
+def is_rule(formula):
+    # TODO
+    #pass
+    if get_head(formula) != get_head(make_or(make_atom("P"), make_atom("P"))):
+        return False
+    if not is_positive_literal(get_conclusion(formula)):
+        return False
+    return all(list(map(is_positive_literal, get_premises(formula))))
+
+def has_args(f):
+    return is_function_call(f) or is_sentence(f)
+
 
 def get_premises(formula):
     return [get_args(n)[0] for n in get_args(formula) if is_negative_literal(n)]
@@ -63,9 +94,13 @@ def is_sentence(f):
     
     return False
 
+# intoarce adevarat daca f este un apel de functie
+def is_function_call(f):
+    return f[0] == "fct"
+
 # intoarce adevarat daca f este un termen
 def is_term(f):
-    return is_constant(f) or is_variable(f)
+    return is_constant(f) or is_variable(f) or is_function_call(f)
 
 # intoarce un termen constant, cu valoarea specificata
 def make_const(value):
@@ -83,6 +118,11 @@ def make_atom(predicate, *args):
 # intoarce o formula care este negarea propozitiei date
 def make_neg(sentence):
     return ("neg", [sentence])
+
+# intoarce un termen care este un apel al functiei cu numele specificat, pe restul argumentelor date.
+# E.g. pentru a construi termenul add[1, 2, 3] vom apela make_function_call(add, 1, 2, 3)
+def make_function_call(name, *args):
+    return ("fct", str(name), list(args))
 
 # intoarce o formula care este conjunctia propozitiilor date
 def make_and(sentence1, sentence2, *others):
@@ -139,6 +179,91 @@ def add_statement(kb, conclusion, *hypotheses):
 
 from functools import reduce
 
+# intoarce formula sau apelul de functie date, in care argumentele au fost inlocuite cu lista new_args
+# e.g. pentru formula p(x, y), inlocuirea argumentelor cu lista [1, 2] va rezulta in formula p(1, 2)
+# Noua lista de argumente trebuie sa aiba aceeasi lungime cu numarul de argumente initial din formula
+def replace_args(formula, new_args):
+    if formula[0] == "fct" or formula[0] == "pred":
+        if len(formula[2]) == len(new_args):
+            return (formula[0], formula[1], new_args)
+    elif formula[0] == "neg" or formula[0] == "and" or formula[0] == "or":
+        if len(formula[1]) == len(new_args):
+            return (formula[0], new_args)
+    return None
+
+# Aplica in formula f toate elementele din substitutia data si intoarce formula rezultata
+def substitute(f, substitution):
+    if substitution is None:
+        return None
+    if is_variable(f) and (get_name(f) in substitution):
+        return substitute(substitution[get_name(f)], substitution)
+    if has_args(f):
+        return replace_args(f, [substitute(arg, substitution) for arg in get_args(f)])
+    return f
+
+from functools import reduce
+
+# Verifica daca variabila v poate fi inlocuita cu termenul t, avand in vedere substitutia subst.
+# Intoarce True daca v poate fi inlocuita cu t, si False altfel.
+# Verificarea esueaza daca, avand in vedere si substitutia, variabila v apare in 
+#  termenul t (deci inlocuirea lui v ar fi un proces infinit).
+def occur_check(v, t, subst, topLevel = True):
+    if is_constant(t):
+        return True
+    elif is_variable(t):
+        if get_name(t) in subst:
+            return occur_check(v, substitute(t, subst), subst, False)
+        else:
+            if topLevel:
+                return True
+            else:
+                if get_name(v) != get_name(t):
+                    return True
+                else:
+                    return False
+    elif is_function_call(t):
+        for arg in get_args(t):
+            if occur_check(v, arg, subst, False) == False:
+                return False
+        return True
+    
+    return True
+
+# Unifica formulele f1 si f2. Rezultatul unificarii este o substitutie (dictionar nume-variabila -> termen),
+#  astfel incat daca se aplica substitutia celor doua formule, rezultatul este identic.
+def unify(f1, f2, subst = None):
+    # TODO
+    #return {}
+    stack = []
+    if subst is None:
+        subst = {}
+    
+    stack.append((f1, f2))
+    
+    while stack:
+        (s, t) = stack.pop()
+        while is_variable(s) and get_name(s) in subst:
+            s = substitute(s, subst)
+        while is_variable(t) and get_name(t) in subst:
+            t = substitute(t, subst)
+        if s != t:
+            if is_variable(s):
+                if not occur_check(s, t, subst):
+                    return False
+                subst[get_name(s)] = t
+            elif is_variable(t):
+                if not occur_check(t, s, subst):
+                    return False
+                subst[get_name(t)] = s
+            elif has_args(s) and has_args(t):
+                if get_head(s) != get_head(t) or len(get_args(s)) != len(get_args(t)):
+                    return False
+                for i in range(len(get_args(s))):
+                    stack.append((get_args(s)[i], get_args(t)[i]))
+            else:
+                return False
+    return subst
+
 # Afiseaza formula f. 
 # Daca argumentul return_result este True, 
 # rezultatul nu este afisat la consola, ci intors.
@@ -169,18 +294,130 @@ def print_formula(f, return_result = False):
     print ret
 
 
+
 def print_KB(KB):
     print("KB now:")
     for s in KB:
         print("\t\t\t" + print_formula(s, True))
 
 
-def get_recursive_pred(formula):
-    p = get_premises(formula)
+# def get_recursive_pred(formula):
+#     p = get_premises(formula)
 
-    for pi in p[0][1]:
-        if not isinstance(pi[1], list):
-                return pi[1]
+#     for pi in p[0][1]:
+#         if not isinstance(pi[1], list):
+#                 return pi[1]
+#         else:
+#             get_recursive_pred(pi[1][1])
+
+def equal_terms(args1, args2):
+    if len(args1) != len(args2):
+        # Predicatele au aritate diferita
+        return False
+    for i, arg in enumerate(args2):
+        if is_constant(arg):
+            if not is_constant(args1[i]) or get_value(args1[i]) != get_value(arg):
+                return False
+        if is_variable(arg):
+            if not is_variable(args1[i]) or get_name(args1[i]) != get_name(arg):
+                return False
+        if is_function_call(arg):
+            if not is_function(args1[i]) or get_head(args1[i]) != get_head(arg):
+                return False
+            if not equal_terms(get_args(args1[i]), get_args(arg)):
+                return False
+    return True
+
+def is_equal_to(a1, a2):
+    # Nu verifica functii
+    if not is_atom(a1):
+        # a1 nu este atom
+        return False
+    if get_head(a1) != get_head(a2):
+        # Predicatele au nume diferite
+        return False
+    return equal_terms(get_args(a1), get_args(a2))
+
+
+def forward_chaining(kb, theorem, verbose=True):
+    # Salvam baza de date originala, lucram cu o copie
+    local_kb = deepcopy(kb)
+    # Doua variabile care descriu starea cautarii
+    got_new_facts = True   # s-au gasit fapte noi la ultima cuautare
+    is_proved = False      # a fost demostrata teorema
+    # Verificam daca teorema este deja demonstata
+    for fact in filter(is_fact, local_kb):
+        if is_equal_to(fact, theorem):
+            return True
+        if unify(fact, theorem):
+            if verbose:
+                print "This already in KB: "
+                print_formula(fact)
+            is_proved = True
+            break
+    while (not is_proved) and got_new_facts:
+        got_new_facts = False
+        for rule in filter(is_rule, local_kb):
+            # Pentru fiecare regula
+            new_facts = apply_rule(rule, list(filter(is_fact, local_kb)))
+            new_facts = list(filter(lambda fact: not any(list(filter(lambda orig: is_equal_to(fact, orig), local_kb))), new_facts))
+            if new_facts:
+                if verbose:
+                    print "Applied rule: "
+                    print_formula(rule)
+                got_new_facts = True
+                for fact in new_facts:
+                    #if verbose:
+                    #    print("New fact: ", end = "")
+                    #    print_formula(fact)
+                    if unify(fact, theorem) != False:
+                        is_proved = True
+                        add_statement(local_kb, fact)
+                        if verbose:
+                            print "Now in KB: "
+                            print_formula(fact)
+                        break
+                    add_statement(local_kb, fact)
+            if is_proved:
+                break
+    if verbose:
+        if is_proved:
+            print "The theorem is TRUE!"
         else:
-            get_recursive_pred(pi[1][1])
+            print "The theorem is FALSE!"
+    return is_proved
 
+def apply_rule_2(rule, facts):
+    if is_fact(rule):
+        return [rule]
+    solutions = []
+    premises = get_premises(rule)
+    premise = premises.pop()
+    rest = make_statement(get_conclusion(rule), premises)
+    for fact in facts:
+        s = unify(premise, fact)
+        if s != False:
+            solutions.extend(apply_rule_2(substitute(rest, s), facts))
+    return solutions
+
+def get_all_solutions(premises, facts, subst):
+    if premises:
+        all_solutions = []
+        for f in facts:
+            new_subst = unify(premises[0], f, deepcopy(subst))
+            if new_subst != False:
+                all_solutions.extend(get_all_solutions(premises[1:], facts, new_subst))
+    else:
+        all_solutions = [subst]
+    return all_solutions
+        
+def apply_rule(rule, facts):
+    # TODO
+    #return []
+    
+    # varianta 1
+    #return list(map(lambda s: substitute(deepcopy(get_conclusion(rule)), s), get_all_solutions(get_premises(rule), facts, {})))
+
+    # varianta 2
+    return apply_rule_2(rule, facts)
+    
